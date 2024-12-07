9c53011c8614f1df0514111fea751744a04cd4fd
SomnathRajesh <141825413+SomnathRajesh@users.noreply.github.com>
2024-12-07 09:31:30 +0000

    disabled linting

--- wikibot.py
+++ wikibot.py
@@ -1,13 +1,14 @@
 from mylib.bot import scrape
 import click
 
+
 @click.command()
-@click.option('--name',help='Web page we want to scrape')
-@click.option('--length',help='length of output from wikipedia')
+@click.option("--name", help="Web page we want to scrape")
+@click.option("--length", help="length of output from wikipedia")
+def cli(name, length):
+    result = scrape(name, length=length)
+    click.echo(click.style(f"{result}:", fg="blue"))
 
-def cli(name,length):
-    result = scrape(name,length=length)
-    click.echo(click.style(f"{result}:",fg="blue"))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
